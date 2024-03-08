4eff4609f74fc25c393c3ad08908979e2ba25873
Tyler-McIntyre <tmcintyre802@gmail.com>
2024-03-08 10:36:11 -0500

    Commit to repo

---
+++ api/service.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+
+	current_weather "github.com/tyler-mcintyre/go_client/clients/current-weather"
+	models "github.com/tyler-mcintyre/go_client/models"
+)
+
+type Service interface {
+	GetCurrentWeather(lat, lon string) (models.CurrentWeatherResponse, error)
+}
+
+type service struct {
+	currentWeatherClient current_weather.Client
+}
+
+func NewService(cwc current_weather.Client) service {
+	return service{
+		currentWeatherClient: cwc,
+	}
+}
+
+func (s service) GetCurrentWeather(lat, lon string) (models.CurrentWeatherResponse, error) {
+	response, err := s.currentWeatherClient.GetCurrentWeather(lat, lon)
+	if err != nil {
+		return models.CurrentWeatherResponse{}, err
+	}
+
+	var currentWeatherResponse models.CurrentWeatherResponse
+	currentWeatherResponse.City = response.Name
+
+	var weatherDesc string
+	for i, value := range response.Weather {
+		if i == 0 {
+			weatherDesc += value.Description
+
+		} else {
+			weatherDesc += "," + value.Description + ","
+		}
+		currentWeatherResponse.Condition = append(currentWeatherResponse.Condition, value.Description)
+	}
+
+	var feelsLike string
+	switch {
+	case response.Feels_Like < 40:
+		feelsLike = "cold"
+	case response.Feels_Like >= 40 && response.Feels_Like < 50:
+		feelsLike = "cool"
+	case response.Feels_Like >= 50 && response.Feels_Like < 70:
+		feelsLike = "slightly cool"
+	case response.Feels_Like >= 70 && response.Feels_Like < 80:
+		feelsLike = "slightly warm"
+	case response.Feels_Like >= 80 && response.Feels_Like < 85:
+		feelsLike = "warm"
+	case response.Feels_Like >= 85:
+		feelsLike = "hot"
+	}
+
+	currentWeatherResponse.Temperature = response.Temp
+
+	currentWeatherResponse.Overview = fmt.Sprintf("It's currently %.0f degrees outside and %s with conditions of %s and %.0f mph winds. "+
+		"Today's high will be %.0f, with a low of %.0f, and it feels like %.0f degrees.", response.Temp, feelsLike, weatherDesc,
+		response.Speed, response.Main.Temp_Max, response.Main.Temp_Min, response.Main.Feels_Like)
+
+	return currentWeatherResponse, nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tyler-mcintyre/go_service_layer
+
+go 1.22.1
+
+require github.com/tyler-mcintyre/go_client v0.0.0-20240308150007-11cb3c85787d
